d4f4e6d546e77be6ce18172a4bd8a3f12e32202d
freshprinzce <biodun.olayinka@gmail.com>
2023-02-19 16:42:36 -0800

    print 5

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty sytle doc for function main goes there */
+/**
+ * main i main function
+ int main(void)
+ {
+ int n;
+
+ strand(time(0));y
+ n = rand() -RAND_MAX / 2;
