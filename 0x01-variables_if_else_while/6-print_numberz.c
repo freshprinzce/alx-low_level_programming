c188575b1a0a8284233cbfa1076af3b0d3c6be6b
freshprinzce <biodun.olayinka@gmail.com>
2023-02-20 16:05:21 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - Prints numbers from 0 to 9.
  *
- * Return: always (0)
+ * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-	putchar( i + '0');
-putchar('\n');
-	Return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
