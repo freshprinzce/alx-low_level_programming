0e6a4461ad194a65ffd51c5650d696f357a27d02
freshprinzce <biodun.olayinka@gmail.com>
2023-02-27 14:26:35 -0800

    4 print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+
+	_putchar('\n');
+}
