dd734600c045a609f69e000421ec4acdfdf24b16
freshprinzce <biodun.olayinka@gmail.com>
2023-02-27 14:38:27 -0800

    5 rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: sring parameter input
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+	char ch;
+
+	for (a = 0; s[a] != '\0'; ++a)
+		;
+
+	for (b = 0; b < a / 2; ++b)
+	{
+		ch = s[b];
+		s[b] = s[a - 1 - b];
+		s[a - 1 - b] = ch;
+	}
+}
